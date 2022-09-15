2cd501e5e71e55a05451054562ac7286f284b231
fryguy100 <eric.tenenbown@gmail.com>
2022-09-15 16:52:03 -0500

    remove the unused vpc module block

--- main.tf
+++ main.tf
@@ -310,22 +310,3 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "3.4.0"
 }
-
-module "vpc" {
-  source             = "terraform-aws-modules/vpc/aws"
-  version            = "3.14.4"
-  name               = "my-vpc-terraform"
-  cidr               = var.vpc_cidr
-  azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-  tags = {
-    Name        = "VPC from Module"
-    Terraform   = "true"
-    Environment = "development"
-  }
-}
-
-
