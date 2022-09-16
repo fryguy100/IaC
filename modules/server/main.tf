26513aebf041430087849345dbd2d2daae0dbe8a
fryguy100 <eric.tenenbown@gmail.com>
2022-09-16 16:44:57 -0500

    further local var consolidation and adding common tags

--- modules/server/main.tf
+++ modules/server/main.tf
@@ -1,9 +1,21 @@
-
 locals {
+  server_name  = var.identity
   service_name = "Automation"
   app_team     = "Cloud Team"
   createdby    = "terraform"
+  application = "front end web server"
 }
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    Name      = local.server_name
+    App       = local.application
+    Service   = local.service_name
+    AppTeam   = local.app_team
+    CreatedBy = local.createdby
+  }
+}
+
 
 resource "aws_instance" "web" {
   ami                    = var.ami
@@ -11,12 +23,5 @@ resource "aws_instance" "web" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
   key_name               = var.key_name
-  tags = {
-    "Identity"    = var.identity
-    "Name"        = var.identity
-    "Environment" = "Training"
-    "Service"     = local.service_name
-    "App Team"    = local.app_team
-    "CreatedBy"   = local.createdby
-  }
+  tags                   = local.common_tags
 }
--- modules/web_server/main.tf
+++ modules/web_server/main.tf
@@ -1,8 +1,19 @@
-
 locals {
+  server_name  = var.identity
   service_name = "Web-App"
   app_team     = "Cloud Team"
   createdby    = "terraform"
+  application = "front end web server"
+}
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    Name      = local.server_name
+    App       = local.application
+    Service   = local.service_name
+    AppTeam   = local.app_team
+    CreatedBy = local.createdby
+  }
 }
 
 resource "aws_instance" "web" {
@@ -25,13 +36,5 @@ resource "aws_instance" "web" {
       "sudo sh /tmp/assets/setup-web.sh",
     ]
   }
-
-  tags = {
-    "Identity"    = var.identity
-    "Name"        = var.identity
-    "Environment" = "Training"
-    "Service"     = local.service_name
-    "App Team"    = local.app_team
-    "CreatedBy"   = local.createdby
-  }
+  tags = local.common_tags
 }
